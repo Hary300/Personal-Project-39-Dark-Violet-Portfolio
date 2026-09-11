fbd07a4d7bcff1be48ec138733a2622b6b0d2e54
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-11 09:26:19 +0700

    feat: enhance ComparisonSection with motion effects; integrate fadeInUp animation for improved visual presentation

--- src/sections/04-comparison/ComparisonSection.tsx
+++ src/sections/04-comparison/ComparisonSection.tsx
@@ -1,6 +1,9 @@
 import SectionWrapper from '@/components/layouts/SectionWrapper';
 import SectionTitle from '@/components/shared/SectionTitle';
 import { comparisonData } from '@/data/04-comparisonData';
+import { fadeInUp } from '@/motions/fadeInUp';
+
+import { motion } from 'motion/react';
 
 const ComparisonSection = () => {
   const tableHeaders = comparisonData.tableHeaders;
@@ -11,7 +14,13 @@ const ComparisonSection = () => {
   return (
     <SectionWrapper sectionId='comparison' className='bg-neutral-100'>
       <SectionTitle title={comparisonData.title} />
-      <div className='flex flex-col rounded-xl lg;rounded-2xl bg-white py-4 px-3 lg:py-6 lg:px-6'>
+      <motion.div
+        variants={fadeInUp}
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        className='flex flex-col rounded-xl lg;rounded-2xl bg-white py-4 px-3 lg:py-6 lg:px-6'
+      >
         <div className='grid grid-cols-[1fr_0.25fr_0.25fr] md:grid-cols-3 text-white bg-primary-200 rounded-full justify-items-center h-14 items-center'>
           {tableHeaders.map((header, index) => (
             <p key={index} className='font-semibold text-sm lg:text-lg px-4'>
@@ -49,7 +58,7 @@ const ComparisonSection = () => {
             </div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </SectionWrapper>
   );
 };
--- src/sections/05-project/ProjectSection.tsx
+++ src/sections/05-project/ProjectSection.tsx
@@ -1,11 +1,53 @@
 import SectionWrapper from '@/components/layouts/SectionWrapper';
 import SectionTitle from '@/components/shared/SectionTitle';
 import { projectsData } from '@/data/05-projectsData';
+import { fadeInUp } from '@/motions/fadeInUp';
+import { motion } from 'motion/react';
 
 const ProjectSection = () => {
+  const projects = projectsData.projects;
+  const ArrowIcon = projectsData.arrowReactIcon;
   return (
     <SectionWrapper sectionId='projects'>
       <SectionTitle title={projectsData.title} />
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-5'>
+        {projects.map((project) => (
+          <motion.div
+            variants={fadeInUp}
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            key={project.id}
+            className='flex flex-col gap-4 lg:gap-5 h-full justify-between'
+          >
+            <div className='flex flex-col gap-4 lg:gap-5'>
+              <div className='flex justify-between'>
+                <p className='py-1 px-4 flex rounded-full border border-dashed text-xs lg:text-sm'>
+                  {project.category}
+                </p>{' '}
+                <p className='py-1 px-4 flex rounded-full border border-dashed text-xs lg:text-sm'>
+                  {project.year}
+                </p>
+              </div>
+              <div className='rounded-xl lg:rounded-2xl overflow-hidden'>
+                <img src={project.image} alt={project.altText} />
+              </div>
+              <p className='font-semibold text-md lg:text-xl'>
+                {project.title}
+              </p>
+            </div>
+            <a
+              href={project.link}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-primary-300 text-sm lg:text-md font-medium flex items-center gap-2'
+            >
+              <span>Visit Website</span>
+              <ArrowIcon />
+            </a>
+          </motion.div>
+        ))}
+      </div>
     </SectionWrapper>
   );
 };
